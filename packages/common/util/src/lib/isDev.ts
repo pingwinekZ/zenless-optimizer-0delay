d1a87d89abd333768088e894ea01afb39bf97038
pingwinekZ <pingwinekzlibanu@gmail.com>
2026-07-02 14:59:46 +0200

    fix hujix mujix

--- packages/common/util/src/lib/isDev.ts
+++ packages/common/util/src/lib/isDev.ts
@@ -1,7 +1,7 @@
-export const isDev = import.meta.env.DEV
+export const isDev = process.env['NODE_ENV'] === 'development'
 
 /**
  * Boolean indicating if dev components should be shown
  */
 export const shouldShowDevComponents =
-  isDev || import.meta.env['VITE_NX_SHOW_DEV_COMPONENTS'] === 'true'
+  isDev || process.env['VITE_NX_SHOW_DEV_COMPONENTS'] === 'true'
