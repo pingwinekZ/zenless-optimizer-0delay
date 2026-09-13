82cf80ae7c74720bdb7b3402c17bcc31cfae19d5
pingwinekZ <164920618+pingwinekZ@users.noreply.github.com>
2026-09-13 07:30:50 +0200

    ben

--- app/src/formula-ui/char/sheets/Ben.tsx
+++ app/src/formula-ui/char/sheets/Ben.tsx
@@ -1,7 +1,15 @@
+import { ColorText } from '@zenless-optimizer/common/ui'
 import type { CharacterKey } from '../../../consts'
 import { Ben } from '../../../formula'
-import { st, trans } from '../../util'
-import { createBaseSheet, fieldForBuff } from '../sheetUtil'
+import { GameDesc } from '../../../i18n'
+import { trans } from '../../util'
+import {
+  AbilityBodyText,
+  CoreGameDesc,
+  createBaseSheet,
+  fieldForBuff,
+} from '../sheetUtil'
+import { getVariant } from '../util'
 
 const key: CharacterKey = 'Ben'
 const [, ch] = trans('char', key)
@@ -10,74 +18,44 @@ const buff = Ben.buffs
 const formula = Ben.formulas
 
 const sheet = createBaseSheet(key, {
-  perSkillAbility: {
-    special: {
-      SpecialAttackFiscalFist: [
-        {
-          type: 'fields',
-          fields: [
-            {
-              title: st('shield'),
-              fieldRef: formula.special_shield.tag,
-            },
-          ],
-        },
-      ],
-      EXSpecialAttackCashflowCounter: [
-        {
-          type: 'fields',
-          fields: [
-            {
-              title: st('shield'),
-              fieldRef: formula.special_shield.tag,
-            },
-          ],
-        },
-      ],
-    },
-  },
   core: [
     {
       type: 'fields',
-      fields: [
-        fieldForBuff(buff.core_atk),
-        {
-          title: st('shield'),
-          fieldRef: formula.core_shield.tag,
-        },
-      ],
+      description: <CoreGameDesc characterKey={key} paragraph={0} />,
+      header: { icon: null, text: ch('core_header') },
+      fields: [fieldForBuff(buff.core_atk)],
     },
   ],
   ability: [
     {
       type: 'conditional',
       conditional: {
-        label: ch('abilityCond'),
-        description:
-          "While Ben's Guardian shield is active, he gains increased CRIT Rate.",
+        label: ch('shieldCond'),
+        description: (
+          <>
+            <GameDesc ns="char_Ben_gen" key18="ability.desc.0" />
+            <AbilityBodyText characterKey={key}>
+              <GameDesc ns="char_Ben_gen" key18="ability.desc.1" />
+            </AbilityBodyText>
+          </>
+        ),
         metadata: cond.shieldOn,
         fields: [fieldForBuff(buff.ability_crit_)],
-      },
-    },
-  ],
-  m1: [
-    {
-      type: 'conditional',
-      conditional: {
-        label: ch('m1Cond'),
-        description:
-          'When successfully blocking an enemy, Ben reduces incoming damage.',
-        metadata: cond.enemyBlocked,
-        fields: [fieldForBuff(buff.m1_dmg_red_)],
       },
     },
   ],
   m2: [
     {
       type: 'fields',
+      description: <GameDesc ns="char_Ben_gen" key18="mindscapes.2.desc" />,
+      header: { icon: null, text: ch('m2_header') },
       fields: [
         {
-          title: st('dmg'),
+          title: (
+            <ColorText color={getVariant(formula.m2_dmg.tag)}>
+              {ch('m2_dmg')}
+            </ColorText>
+          ),
           fieldRef: formula.m2_dmg.tag,
         },
       ],
@@ -87,13 +65,24 @@ const sheet = createBaseSheet(key, {
     {
       type: 'conditional',
       conditional: {
-        label: ch('m1Cond'),
-        description:
-          'When successfully blocking an enemy, Ben gains increased Counter DMG.',
-        metadata: cond.enemyBlocked,
+        label: ch('m4Cond'),
+        description: <GameDesc ns="char_Ben_gen" key18="mindscapes.4.desc" />,
+        metadata: cond.enemyBlocked_m4,
         fields: [
           {
-            title: ch('m4_dmg_'),
+            title: (
+              <ColorText color={getVariant(buff.m4_dmg_.tag)}>
+                {ch('m4_fiscal_fist_dmg_')}
+              </ColorText>
+            ),
+            fieldRef: buff.m4_dmg_.tag,
+          },
+          {
+            title: (
+              <ColorText color={getVariant(buff.m4_dmg_.tag)}>
+                {ch('m4_cashflow_counter_dmg_')}
+              </ColorText>
+            ),
             fieldRef: buff.m4_dmg_.tag,
           },
         ],
@@ -105,8 +94,7 @@ const sheet = createBaseSheet(key, {
       type: 'conditional',
       conditional: {
         label: ch('m6Cond'),
-        description:
-          'After launching an attack during EX Special Attack, Ben gains increased Daze from subsequent attacks.',
+        description: <GameDesc ns="char_Ben_gen" key18="mindscapes.6.desc" />,
         metadata: cond.attackLaunched,
         fields: [
           fieldForBuff(buff.m6_basic_dazeInc_),
