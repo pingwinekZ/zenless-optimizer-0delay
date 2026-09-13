31be4bfa0a681b646b578674c6d1b0dc0ea24243
pingwinekZ <164920618+pingwinekZ@users.noreply.github.com>
2026-09-13 11:31:39 +0200

    fwaf

--- app/src/page-home/index.tsx
+++ app/src/page-home/index.tsx
@@ -1,19 +1,5 @@
-import {
-  Alert,
-  Anchor,
-  CardSection,
-  Flex,
-  Group,
-  SimpleGrid,
-  Title,
-} from '@mantine/core'
-import { IconFileDescription, IconMessageReport } from '@tabler/icons-react'
-import { CardThemed } from '@zenless-optimizer/common/ui'
-import { useEffect, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import { ZCard } from '../ui'
+import { Alert, Anchor, Flex, SimpleGrid, Title } from '@mantine/core'
+import { IconMessageReport } from '@tabler/icons-react'
 import { IntroCard } from './IntroCard'
 import QuickLinksCard from './QuickLinksCard'
 import { Roadmap } from './Roadmap'
@@ -37,7 +23,6 @@ function FeedbackBanner() {
   )
 }
 
-declare const __VERSION__: string
 export default function PageHome() {
   return (
     <Flex direction="column" gap="md" maw={1400} w="100%" mx="auto">
@@ -46,9 +31,6 @@ export default function PageHome() {
       <SimpleGrid cols={{ base: 1, lg: 3 }} spacing="md" verticalSpacing="md">
         <Flex direction="column" gap="md" style={{ gridColumn: 'span 2' }}>
           <Roadmap />
-          <ZCard>
-            <PatchNotesCard />
-          </ZCard>
         </Flex>
         <Flex direction="column" gap="md">
           <QuickLinksCard />
@@ -56,39 +38,5 @@ export default function PageHome() {
         </Flex>
       </SimpleGrid>
     </Flex>
-  )
-}
-
-function PatchNotesCard() {
-  const { t } = useTranslation('page_home')
-  const [{ isLoaded, text }, setState] = useState({ isLoaded: false, text: '' })
-  useEffect(() => {
-    const regex = /^(\d+)\.(\d+)\.(\d+)$/
-    const minorVersion = __VERSION__.replace(regex, `$1.$2.${0}`)
-    fetch(process.env['NX_URL_GITHUB_API_GO_RELEASES'] + minorVersion)
-      .then((res) => res.arrayBuffer())
-      .then((buffer) => {
-        const decoder = new TextDecoder('utf-8')
-        const data = decoder.decode(buffer)
-        const release = JSON.parse(data)
-        setState({ isLoaded: true, text: release.body })
-      })
-      .catch((err) => console.log('Error: ' + err.message))
-  }, [])
-
-  return (
-    <CardThemed>
-      <Group p="md" style={{ paddingBottom: 0 }}>
-        <IconFileDescription />
-        <Title order={5}>{t('quickLinksCard.buttons.patchNotes.title')}</Title>
-      </Group>
-      <CardSection>
-        {isLoaded ? (
-          <ReactMarkdown children={text} remarkPlugins={[remarkGfm]} />
-        ) : (
-          'Loading...'
-        )}
-      </CardSection>
-    </CardThemed>
   )
 }
